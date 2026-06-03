dbce390d414424b7667bcc56bdd9f293aaadea72
Ranjit <ranjitraut877@gmail.com>
2026-06-03 17:34:50 +0545

    page ma link bata pdf halyo vani uta view ma iframe ma khulako

--- classes/lesson/Page.php
+++ classes/lesson/Page.php
@@ -2,12 +2,10 @@
 
 namespace theme_mytheme\lesson;
 
-
 use theme_mytheme\lesson\contracts\LessonModuleInterface;
 
 class Page implements LessonModuleInterface
 {
-
     protected $cm;
     protected $DB;
 
@@ -19,9 +17,9 @@ class Page implements LessonModuleInterface
 
     public function getData(): array
     {
-        global $CFG;
-
-        $page = $this->DB->get_record('page', ['id' => $this->cm->instance]);
+        $page = $this->DB->get_record('page', [
+            'id' => $this->cm->instance
+        ]);
 
         $content = '';
 
@@ -29,6 +27,7 @@ class Page implements LessonModuleInterface
 
             $context = \context_module::instance($this->cm->id);
 
+            // Replace @@PLUGINFILE@@ URLs
             $content = file_rewrite_pluginfile_urls(
                 $page->content,
                 'pluginfile.php',
@@ -38,10 +37,43 @@ class Page implements LessonModuleInterface
                 0
             );
 
+            // Moodle formatting
             $content = format_text(
                 $content,
                 $page->contentformat,
                 ['context' => $context]
+            );
+
+            // Convert PDF links into iframe viewers
+            $content = preg_replace_callback(
+                '/<a[^>]+href=["\']([^"\']+\.pdf(?:\?[^"\']*)?)["\'][^>]*>.*?<\/a>/is',
+                function ($matches) {
+
+                    $pdfurl = html_entity_decode($matches[1]);
+
+                    // Remove forced download if present
+                    $pdfurl = preg_replace(
+                        '/([?&])forcedownload=1/',
+                        '$1forcedownload=0',
+                        $pdfurl
+                    );
+
+                    if (strpos($pdfurl, 'forcedownload=') === false) {
+                        $pdfurl .= (strpos($pdfurl, '?') !== false ? '&' : '?')
+                            . 'forcedownload=0';
+                    }
+
+                    return '
+                    <div class="pdf-viewer-wrapper" style="margin:20px 0;">
+                        <iframe
+                            src="' . $pdfurl . '"
+                            width="100%"
+                            height="800"
+                            style="border:1px solid #ddd;border-radius:8px;">
+                        </iframe>
+                    </div>';
+                },
+                $content
             );
         }
 
